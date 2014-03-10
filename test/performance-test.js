c2c957800a2e0416042be967ee053726993b8fed
guy <guy@gadp.ch>
2014-03-10 22:31:07 +0100

    Extended test to check scroll position on smaller data set

--- test/performance-test.js
+++ test/performance-test.js
@@ -14,7 +14,21 @@ angular.module('ngcTableDirectiveTest', ['ngcTableDirective'])
                }
                 $scope.data.push(rowContent);
             }
-        });
+        }, 2000);
+
+        $timeout(function() {
+            var rows = parseInt($location.search().rows) || 100;
+            var cols = parseInt($location.search().cols) || 100;
+
+            $scope.data = [];
+            for (var row = 0; row < rows; row++) {
+                var rowContent = [];
+                for (var col = 1; col < cols + 1; col++) {
+                    rowContent.push(row * col + col);
+                }
+                $scope.data.push(rowContent);
+            }
+        }, 5000);
 
     });
 
